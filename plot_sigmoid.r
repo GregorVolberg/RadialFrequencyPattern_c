30d8aa9736beaf830eebb7720d76acb702e0f161
Gregor Volberg <gregor.volberg@ur.de>
2022-05-03 10:08:35 +0200

    comt

--- plot_sigmoid.r
+++ plot_sigmoid.r
@@ -21,16 +21,16 @@ vpnames = unique(ds$vp)
 # get fraction 'spiky' responses  
 prz1all = ds %>% group_by(vp, nodd, waveform) %>%
       summarize(f_spikey = mean(isRound == 0)) %>%
-      filter(waveform == 'sine') %>% 
+      filter(waveform == '0.5') %>% 
       ungroup()
 
 prz2all = ds %>% group_by(vp, nodd, waveform) %>%
        summarize(f_spikey = mean(isRound == 0)) %>%
-       filter(waveform == 'tria') %>% 
+       filter(waveform == '1.5') %>% 
        ungroup()
 
 for (k in 1:length(vpnames)){
-PngFileName = paste(PngPath, 'RFPb-', vpnames[k], '.png', sep = '')
+PngFileName = paste(PngPath, 'RFPc-', vpnames[k], '.png', sep = '')
 png(filename = PngFileName,
     width  = 360,
     height = 360)
@@ -43,7 +43,7 @@ z_spike = qnorm(spike)
 erg = lm(z_spike ~ prz1$nodd)
 m = -(coef(erg)[1])/coef(erg)[2]
 s = 1 / coef(erg)[2]
-curve(pnorm(x,m,s), 0,3, col = 'red', xlim=c(0,3), ylim=c(0,1), ylab = 'p spiky', xlab = 'n odd-symmetrics', lwd=2)
+curve(pnorm(x,m,s), 0,12, col = 'red', xlim=c(0,12), ylim=c(0,1), ylab = 'p spiky', xlab = 'n odd-symmetrics', lwd=2)
 points(prz1$nodd, prz1$f_spikey, col='red', cex=2)
 
 prz2  = prz2all %>% filter(vp == vpnames[k])
@@ -54,13 +54,11 @@ z_spike = qnorm(spike)
 erg = lm(z_spike ~ prz2$nodd)
 m = -(coef(erg)[1])/coef(erg)[2]
 s = 1 / coef(erg)[2]
-curve(pnorm(x,m,s), 0,3, col = 'blue', xlim=c(0,3), ylim=c(0,1), add=T, lwd=2)
+curve(pnorm(x,m,s), 0,12, col = 'blue', xlim=c(0,12), ylim=c(0,1), add=T, lwd=2)
 points(prz1$nodd, prz2$f_spikey, col='blue', cex=2)
 
 text(2.5,0.1, vpnames[k])
-legend(1,0.4,legend = c('sine', 'triangle'), lty = c('solid', 'solid'),col = c('red', 'blue'), bty='n')
+legend(1,0.4,legend = c('0.5', '1.5'), lty = c('solid', 'solid'),col = c('red', 'blue'), bty='n')
 dev.off()
 }
 
-
-# V10, 13 , 15 auf Rateniveau
